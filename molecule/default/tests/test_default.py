90c3bed3f19fc035acab238cbb9efd1c26be071f
Ernesto Valero <evalero@paradigmadigital.com>
2018-03-28 19:03:03 +0200

    First commit Added molecule test Added scenarios to test in centos and debian Created tasks for main role Created vars depending of OS family

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,11 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_atom_installed(host):
+    f = host.file('/bin/atom')
+    assert f.exists
